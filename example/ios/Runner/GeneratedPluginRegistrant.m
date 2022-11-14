0726fe17d0d18083dbaa7f5dad54f303b348e784
mihir-trt <mihirp@thirdrocktechkno.com>
2022-11-14 18:13:06 +0530

    Init Commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<edge_detection/EdgeDetectionPlugin.h>)
+#import <edge_detection/EdgeDetectionPlugin.h>
+#else
+@import edge_detection;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [EdgeDetectionPlugin registerWithRegistrar:[registry registrarForPlugin:@"EdgeDetectionPlugin"]];
+}
+
+@end
